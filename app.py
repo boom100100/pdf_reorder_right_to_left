db39b1c2096a3d683822d0e07a9451303928ccdb
bernadette <bsd231@nyu.edu>
2025-11-12 12:10:20 -0500

    added multifile picker

--- app.py
+++ app.py
@@ -1,65 +1,84 @@
 #!/usr/bin/python3
-"""Reorder PDF pages.
+"""Reorder PDF pages for right-to-left reading.
 
 Use case: a manga PDF on two-page view shows the pages on the wrong side.
 
-The fix here is to move the even page behind the odd page.
+The fix here is to move the even page behind the following odd page.
 """
 
 from PyPDF2 import PdfReader, PdfWriter
-import tkinter as tk
-from tkinter import ttk
 from tkinter import filedialog as fd
-from tkinter.messagebox import showinfo
 
-# create the root window
-root = tk.Tk()
+def process_reorder(source, output_filename):
+    pdf = PdfReader(source)
+    pages = pdf.pages
+    pdf_writer = PdfWriter()
 
+    previous_page = None
 
+    for page_number in range(len(pages)):
+        current_page = pages[page_number]
+        if page_number % 2 == 0: # modulo distinguishes even value, but for a PDF, page 0 is the odd-numbered page
+            pdf_writer.add_page(current_page)
+        elif previous_page is not None:
+            pdf_writer.add_page(previous_page)
+            previous_page = current_page # moves the even-numbered page to the right of the odd-numbered page.
+        else:
+            previous_page = current_page
 
-source = fd.askopenfilename(
-    title='Open a file',
-    initialdir='.',
-    filetypes=(
-    ('pdf files', '*.pdf'),
-))
-print(f"Source: {source}\n")
+    # Write the data to disk
+    with open(output_filename, "wb") as out:
+        pdf_writer.write(out)
+        print("created", output_filename)
 
-# Make this a file input dialog
-source = "/Users/bernadette/Downloads/black jack ni yoroshiku/001bj.pdf"
-pdf = PdfReader(source)
-pages = pdf.pages
+def get_names(file_path: str, len_of_sources: int, sources: list[str]) -> tuple[str, str]:
+    """Get name-related values for output files."""
 
-destination = fd.askdirectory(initialdir=source)
-print(f"Destination:\n{destination}\n")
+    # multiple file processing
+    default_append_name = "-updated"
+    append_name = None
 
-last_forward_slash = source.rfind("/")
-last_period = source.rfind(".")
-default_output_filename = f"{source[last_forward_slash + 1: last_period]}-updated"
-output_filename = input(f"Please select a file name. Do not include the PDF extension.\nDefault value: {default_output_filename}")
-print(f"Output filename:\n{output_filename=} Is empty?: {output_filename == ""}\n")
-output_filename = output_filename or default_output_filename
-print(f"Final output filename:\n{output_filename=}\n")
-output_filename = f"{destination}/{output_filename}.pdf"
-# Make this file output directory dialog and name input (or all-in-one, if possible).
-output_filename = "/Users/bernadette/Downloads/black jack ni yoroshiku/001bj-updated.pdf"
+    last_forward_slash = file_path.rfind("/")
+    last_period = file_path.rfind(".")
+    default_output_filename = f"{sources[0][last_forward_slash + 1: last_period]}-updated"
+    output_filename = None
 
-pdf_writer = PdfWriter()
-previous_page = None
-
-# Get reach page and add it to corresponding
-# output file based on page number
-for page_number in range(len(pages)):
-    current_page = pages[page_number]
-    if page_number % 2 == 0: # modulo distinguishes even value, but for a PDF, page 0 is the odd-numbered page
-        pdf_writer.add_page(current_page)
-    elif previous_page is not None:
-        pdf_writer.add_page(previous_page)
-        previous_page = current_page
+    if len_of_sources > 1:
+        append_name = input(f"Choose value to append to file names.\nThe default value is '{default_append_name}'.\n")
+        append_name = append_name or default_append_name
     else:
-        previous_page = current_page
+        output_filename = input(f"Please input a file name. Do not include the PDF extension.\nDefault value: {default_output_filename}\n")
+        output_filename = output_filename or default_output_filename
 
-# Write the data to disk
-with open(output_filename, "wb") as out:
-     pdf_writer.write(out)
-     print("created", output_filename)
+    return append_name, output_filename
+
+def set_up():
+    sources = fd.askopenfilenames(
+        title='Choose at least one file',
+        initialdir='.',
+        filetypes=(
+        ('pdf files', '*.pdf'),
+    ))
+    len_of_sources = len(sources)
+
+    file_path = sources[0]
+    destination = fd.askdirectory(
+        initialdir=file_path,
+        title="Choose a destination directory for the files"
+    )
+
+    append_name, output_filename = get_names(file_path, len_of_sources, sources)
+    
+    for source in sources:
+        last_forward_slash = source.rfind("/")
+        last_period = source.rfind(".")
+        source_name = source[last_forward_slash + 1: last_period]
+
+        if len_of_sources > 1:
+            output_filename = f"{destination}/{source_name}{append_name}.pdf"
+        else: 
+            output_filename = f"{destination}/{output_filename}.pdf"
+
+        process_reorder(source, output_filename)
+
+set_up()
