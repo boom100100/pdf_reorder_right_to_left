76aacd3afb9587409b843a28322a9fa66d06ea27
bernadette <bsd231@nyu.edu>
2025-11-12 12:36:17 -0500

    clarified getting file name; renamed method to main; added check for file not picked

--- app.py
+++ app.py
@@ -27,38 +27,51 @@ def process_reorder(source, output_filename):
             previous_page = current_page
 
     # Write the data to disk
+    # wb overwrite
+    # xb write if not exists
     with open(output_filename, "wb") as out:
         pdf_writer.write(out)
         print("created", output_filename)
 
-def get_names(file_path: str, len_of_sources: int, sources: list[str]) -> tuple[str, str]:
+
+def get_append_name() -> str:
     """Get name-related values for output files."""
 
     # multiple file processing
     default_append_name = "-updated"
-    append_name = None
+    append_name = input(f"Choose value to append to file names.\nThe default value is '{default_append_name}'.\n")
+    append_name = append_name or default_append_name
+
+    return append_name
+
+
+def get_output_filename(file_path: str, source: str) -> str:
+    """Get name-related values for output files."""
 
     last_forward_slash = file_path.rfind("/")
     last_period = file_path.rfind(".")
-    default_output_filename = f"{sources[0][last_forward_slash + 1: last_period]}-updated"
+    default_output_filename = f"{source[last_forward_slash + 1: last_period]}-updated"
     output_filename = None
 
-    if len_of_sources > 1:
-        append_name = input(f"Choose value to append to file names.\nThe default value is '{default_append_name}'.\n")
-        append_name = append_name or default_append_name
-    else:
-        output_filename = input(f"Please input a file name. Do not include the PDF extension.\nDefault value: {default_output_filename}\n")
-        output_filename = output_filename or default_output_filename
+    output_filename = input(f"Please input a file name. Do not include the PDF extension.\nDefault value: {default_output_filename}\n")
+    output_filename = output_filename or default_output_filename
 
-    return append_name, output_filename
+    return output_filename
 
-def set_up():
+
+def main() -> None:
+    """Reorders a PDF for right-to-left reading on two-page view."""
+
     sources = fd.askopenfilenames(
-        title='Choose at least one file',
-        initialdir='.',
+        title="Choose at least one file",
+        initialdir=".",
         filetypes=(
-        ('pdf files', '*.pdf'),
+        ("pdf files", "*.pdf"),
     ))
+
+    if not sources:
+        raise Exception("There are no PDFs to reorder.")
+
     len_of_sources = len(sources)
 
     file_path = sources[0]
@@ -67,8 +80,10 @@ def set_up():
         title="Choose a destination directory for the files"
     )
 
-    append_name, output_filename = get_names(file_path, len_of_sources, sources)
-    
+    # TODO: this "if" check seems a bit redundant because it is repeated later. But assigning append_name in the for-loop would make for a repetitive UX. And ommitting the "if" flow while still assigning append_name would be confusing in the case of just one PDF, as the app will later request a name that will not take append_name into account.
+    if len_of_sources > 1:
+        append_name = get_append_name()
+
     for source in sources:
         last_forward_slash = source.rfind("/")
         last_period = source.rfind(".")
@@ -77,8 +92,10 @@ def set_up():
         if len_of_sources > 1:
             output_filename = f"{destination}/{source_name}{append_name}.pdf"
         else: 
+            output_filename = get_output_filename()
             output_filename = f"{destination}/{output_filename}.pdf"
 
         process_reorder(source, output_filename)
 
-set_up()
+
+main()
